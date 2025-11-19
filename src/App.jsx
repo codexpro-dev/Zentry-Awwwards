2ad0f159824f2db534f9c852832c4c74e604eac7
codexpro.dev <chimeziev900@gmail.com>
2025-11-18 23:44:03 -0800

    setup initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-blue-700 text-5xl font-bold font-robert-medium'>Welcome to Awwards</h1>
+    </div>
+  )
+}
+
+export default App
